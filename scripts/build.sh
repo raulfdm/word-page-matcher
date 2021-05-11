64de7ba5e97d9d07b9378ab1f0497a331afbee56
Raul Melo <melo.raulf@gmail.com>
2021-05-11 07:57:01 +0200

    refactor: unify chrome and firefox

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-EXTENSION_PATH=extensions
+EXTENSION_DIST_PATH=extension/build
 MAIN_BUILD_PATH=public/build
-CHROME_DIST_PATH=$EXTENSION_PATH/chrome/build
-FIREFOX_DIST_PATH=$EXTENSION_PATH/firefox/build
+
 
 # Clean up
 rm -R public/build
-rm -R $CHROME_DIST_PATH $FIREFOX_DIST_PATH
+rm -R $EXTENSION_DIST_PATH
 
 # Build
 yarn rollup -c
 
 # Move resoureces
-cp -R $MAIN_BUILD_PATH $CHROME_DIST_PATH
-cp -R $MAIN_BUILD_PATH $FIREFOX_DIST_PATH
+cp -R $MAIN_BUILD_PATH $EXTENSION_DIST_PATH
--- scripts/packToPublish.sh
+++ scripts/packToPublish.sh
@@ -2,11 +2,11 @@
 
 ./scripts/build.sh
 
-EXTENSIONS_DIR="$PWD/extensions"
+EXTENSIONS_DIR="$PWD/extension"
 DIST_DIR="$PWD/dist"
 
 # Just make sure we have this folder
 mkdir dist
 
-cd $EXTENSIONS_DIR/firefox && zip -r -FS $DIST_DIR/firefox.zip *
-cd $EXTENSIONS_DIR/chrome && zip -r -FS $DIST_DIR/chrome.zip *
+cd $EXTENSIONS_DIR && zip -r -FS $DIST_DIR/extension.zip *
+
